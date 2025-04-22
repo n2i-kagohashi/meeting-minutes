7c54f5813be557831b2aeec0df28063d66689c35
kagohashi yu-ki <kagohashiyu-ki@Special-george-Mac.local>
2025-04-22 15:08:59 +0900

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const USER = 'your_username' // ← 任意のユーザー名
-const PASS = 'your_password' // ← 任意のパスワード
+const USER = 'your_username' //123
+const PASS = 'your_password' //123
 
 export function middleware(request: NextRequest) {
   const authHeader = request.headers.get('authorization')
